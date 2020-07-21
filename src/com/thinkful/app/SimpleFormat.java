606565eb1ee4e025f96d3352aeeb0bc95277c092
Michael Ploughman <michael@michaelhploughman.com>
2020-07-20 23:01:16 -0600

    String format drill complete

---
+++ src/com/thinkful/app/SimpleFormat.java
@@ -0,0 +1,18 @@
+package com.thinkful.app;
+
+public class SimpleFormat {
+  public static void main(String[] args) {
+    System.out.println("Favorite Movies");
+    System.out.println("***************");
+    System.out.println("1: LOTR");
+    System.out.println(" Viggo Mortensen, Sir Ian McKellen");
+    System.out.println("2: Star Wars");
+    System.out.println(" Luke Skywalker, Han Solo");
+    System.out.println("3: The Matrix");
+    System.out.println(" Keanu Reeves, Carrie Ann Moss");
+    System.out.println("4: Lucky Number Sleven");
+    System.out.println(" Bruce Willis, Lucy Liu");
+    System.out.println("5: Cosmos");
+    System.out.println(" Neil DeGrass Tyson");
+  }
+}
