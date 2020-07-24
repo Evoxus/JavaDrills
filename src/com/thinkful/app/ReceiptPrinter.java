9c8a4ff83c10e7bc9c71b2a07ad99b3caf973245
Michael Ploughman <michael@michaelhploughman.com>
2020-07-23 22:42:56 -0600

    ReceiptPrinter complete

---
+++ src/com/thinkful/app/ReceiptPrinter.java
@@ -0,0 +1,7 @@
+package com.thinkful.app;
+
+public class ReceiptPrinter {
+  public static void main(String[] args) {
+    System.out.println("Bill\n------------------\nPizza       $12.99\nSoda         $1.25\nCookies      $2.50\nSubTotal:   $16.74\nTax(10%):    $1.67\nService(15%%):$2.51\n------------------\nTotal:      $20.92\n------------------");
+  }
+}
