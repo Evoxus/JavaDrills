77689f23e8a3c96ad240d18135611da8c4dbc7cf
Michael Ploughman <michael@michaelhploughman.com>
2020-07-23 22:48:28 -0600

    ChristmasTree printer complete

---
+++ src/com/thinkful/app/ChristmasTree.java
@@ -0,0 +1,7 @@
+package com.thinkful.app;
+
+public class ChristmasTree {
+  public static void main(String[] args) {
+    System.out.println("      *\n     ***\n    *****\n     ***\n    *****\n   *******\n  *********\n   *******\n  *********\n ***********\n*************\n     ***\n     ***\n     ***");
+  }
+}
